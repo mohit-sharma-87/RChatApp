2e3575132a37320212dd06a1e5aae9d9e93b50f1
Mohit Sharma <mohit7575@gmail.com>
2021-05-11 15:32:30 +0100

    Application class added

---
+++ app/src/main/java/com/mongodb/rchatapp/RChatApplication.kt
@@ -0,0 +1,45 @@
+package com.mongodb.rchatapp
+
+import android.app.Application
+import com.mongodb.rchatapp.di.koinModules
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import io.realm.mongodb.App
+import io.realm.mongodb.AppConfiguration
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class RChatApplication : Application() {
+
+    val realmSync by lazy {
+        App(AppConfiguration.Builder(BuildConfig.RealmAppId).build())
+    }
+
+
+    override fun onCreate() {
+        super.onCreate()
+        setupRealm()
+        setupKoin()
+    }
+
+    private fun setupRealm() {
+        Realm.init(this)
+        val config = RealmConfiguration.Builder()
+            .name("rChatDb.db")
+            .allowQueriesOnUiThread(false)
+            .schemaVersion(1)
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(config)
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@RChatApplication)
+            modules(modules = koinModules())
+        }
+    }
+}
