5a618205c00ce63e5dbbd682301ac6a5fe3721dd
Mohit Sharma <mohit7575@gmail.com>
2021-05-11 15:34:47 +0100

    Dependency added in Koin

---
+++ app/src/main/java/com/mongodb/rchatapp/di/KoinModules.kt
@@ -0,0 +1,15 @@
+package com.mongodb.rchatapp.di
+
+import com.mongodb.rchatapp.RChatApplication
+import com.mongodb.rchatapp.ui.home.HomeViewModel
+import com.mongodb.rchatapp.ui.login.LoginViewModel
+import com.mongodb.rchatapp.ui.profile.ProfileViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+fun koinModules() = module {
+    viewModel { HomeViewModel((androidApplication() as RChatApplication).realmSync) }
+    viewModel { ProfileViewModel((androidApplication() as RChatApplication).realmSync) }
+    viewModel { LoginViewModel((androidApplication() as RChatApplication).realmSync) }
+}
