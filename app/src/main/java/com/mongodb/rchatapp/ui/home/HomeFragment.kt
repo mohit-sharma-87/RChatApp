a9a4154371429bb9752a88e173ebec61a03a8719
Mohit Sharma <mohit7575@gmail.com>
2021-05-11 15:37:31 +0100

    Handling for non signed user added

--- app/src/main/java/com/mongodb/rchatapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mongodb/rchatapp/ui/home/HomeFragment.kt
@@ -8,12 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.mongodb.rchatapp.R
 import com.mongodb.rchatapp.databinding.FragmentHomeBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel: HomeViewModel by viewModel()
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,22 +26,32 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        homeViewModel.text.observe(viewLifecycleOwner, {
             textView.text = it
         })
-        return root
+
+        homeViewModel.isLoggedIn.observe(viewLifecycleOwner) {
+            if (!it)
+                openLogin()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun openLogin() {
+        findNavController().navigate(R.id.navigation_login)
+    }
+
 }
--- app/src/main/java/com/mongodb/rchatapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mongodb/rchatapp/ui/home/HomeViewModel.kt
@@ -2,12 +2,30 @@ package com.mongodb.rchatapp.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import io.realm.mongodb.App
+import io.realm.mongodb.User
+import kotlinx.coroutines.flow.DEFAULT_CONCURRENCY
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val realmSync: App) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val _currentUser = MutableLiveData<User>()
+
+    val isLoggedIn: LiveData<Boolean> = Transformations.map(_currentUser) {
+        it != null
+    }
+
+    val isProfileComplete: LiveData<Boolean> = Transformations.map(_currentUser) {
+        true
+    }
+
+    init {
+        _currentUser.value = realmSync.currentUser()
+    }
 }
