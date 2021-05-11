cb51fcdc37dc1abb89ae6de69721d6d8db621532
Mohit Sharma <mohit7575@gmail.com>
2021-05-11 16:40:58 +0100

    bottom controller updated

--- app/src/main/java/com/mongodb/rchatapp/MainActivity.kt
+++ app/src/main/java/com/mongodb/rchatapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mongodb.rchatapp
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -12,6 +13,7 @@ import com.mongodb.rchatapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,17 +21,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        bottomView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_login -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        bottomView.setupWithNavController(navController)
+    }
+
+    private fun showBottomNav() {
+        bottomView.visibility = View.VISIBLE
+    }
+
+    private fun hideBottomNav() {
+        bottomView.visibility = View.GONE
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
