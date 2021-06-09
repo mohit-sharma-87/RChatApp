410c7bfcab0a29c0455ff473175372d05595ea51
Mohit Sharma <mohit7575@gmail.com>
2021-06-09 16:51:22 +0100

    list view implementation

---
+++ app/src/main/java/com/mongodb/rchatapp/ui/chatmembers/ChatMemberListDiffCallback.kt
@@ -0,0 +1,33 @@
+package com.mongodb.rchatapp.ui.chatmembers
+
+import androidx.recyclerview.widget.DiffUtil
+import com.mongodb.rchatapp.ui.data.model.ChatsterListViewModel
+
+class ChatMemberListDiffCallback(
+    private val oldList: List<ChatsterListViewModel>,
+    private val newList: List<ChatsterListViewModel>
+) : DiffUtil.Callback() {
+
+    val ITEM_SELECTION_UPDATED = Any()
+
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition]._id === newList[newItemPosition]._id
+    }
+
+    override fun areContentsTheSame(oldPosition: Int, newPosition: Int): Boolean {
+        return oldList[oldPosition].isSelected == newList[newPosition].isSelected
+    }
+
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+
+        val oldItem = oldList[oldItemPosition]
+        val newItem = newList[newItemPosition]
+
+        return if (oldItem.isSelected == newItem.isSelected) ITEM_SELECTION_UPDATED else null
+    }
+
+}
