c6808e1f4a83c48cc92955e8887cd8a0a4c7c9ed
Mohit Sharma <mohit7575@gmail.com>
2021-06-09 16:51:19 +0100

    list view implementation

--- app/src/main/java/com/mongodb/rchatapp/ui/chatmembers/ChatMemberViewAdapter.kt
+++ app/src/main/java/com/mongodb/rchatapp/ui/chatmembers/ChatMemberViewAdapter.kt
@@ -1,14 +1,17 @@
 package com.mongodb.rchatapp.ui.chatmembers
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.mongodb.rchatapp.databinding.ItemMemberBinding
-import com.mongodb.rchatapp.ui.data.model.Chatster
+import com.mongodb.rchatapp.ui.data.model.ChatsterListViewModel
+import java.util.ArrayList
 
 class ChatMemberViewAdapter : RecyclerView.Adapter<ChatMemberViewAdapter.ViewHolder>() {
 
-    private var values: List<Chatster> = emptyList()
+    private var values: MutableList<ChatsterListViewModel> = ArrayList()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -22,23 +25,49 @@ class ChatMemberViewAdapter : RecyclerView.Adapter<ChatMemberViewAdapter.ViewHol
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.name.text = if (item.displayName.isNullOrEmpty()) {
+
+        holder.binding.icMemberName.text = if (item.displayName.isNullOrEmpty()) {
             item.userName
         } else {
             item.displayName
+        }
+        updateSelectedState(holder.binding.ivCheck, item.isSelected)
+
+        holder.binding.root.apply {
+            setOnClickListener {
+                val updatedItem = item.copy(isSelected = !item.isSelected)
+                val updatedList = values.toMutableList().apply {
+                        this[position] = updatedItem
+                    }
+                updateMemberList(updatedList)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, payloads: MutableList<Any>) {
+        super.onBindViewHolder(holder, position, payloads)
+        if (payloads.isEmpty()) {
+            onBindViewHolder(holder, position)
+        } else {
+            updateSelectedState(holder.binding.ivCheck, values[position].isSelected)
         }
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(binding: ItemMemberBinding) : RecyclerView.ViewHolder(binding.root) {
-        val name = binding.icMemberName
+    inner class ViewHolder(val binding: ItemMemberBinding) : RecyclerView.ViewHolder(binding.root)
+
+    fun updateMemberList(items: List<ChatsterListViewModel>) {
+
+        val diffResult =
+            DiffUtil.calculateDiff(ChatMemberListDiffCallback(oldList = values, newList = items))
+        diffResult.dispatchUpdatesTo(this)
+
+        values.clear()
+        values.addAll(items)
     }
 
-    fun updateMemberList(items: List<Chatster>) {
-        values = items
-        notifyDataSetChanged()
-        //TODO : Implement diff call back
+    private fun updateSelectedState(view: View, isSelected: Boolean) {
+        view.visibility = if (isSelected) View.VISIBLE else View.GONE
     }
-
 }
--- app/src/main/java/com/mongodb/rchatapp/ui/chatmembers/ChatMemberViewModel.kt
+++ app/src/main/java/com/mongodb/rchatapp/ui/chatmembers/ChatMemberViewModel.kt
@@ -2,8 +2,11 @@ package com.mongodb.rchatapp.ui.chatmembers
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.mongodb.rchatapp.ui.data.model.Chatster
+import com.mongodb.rchatapp.ui.data.model.ChatsterListViewModel
+import com.mongodb.rchatapp.ui.data.model.toListViewModel
 import io.realm.Realm
 import io.realm.kotlin.where
 import io.realm.mongodb.App
@@ -12,26 +15,33 @@ import io.realm.mongodb.sync.SyncConfiguration
 class ChatMemberViewModel(private val realmSync: App) : ViewModel() {
 
     private val _members = MutableLiveData<List<Chatster>>()
-    val members: LiveData<List<Chatster>> = _members
+    val members: LiveData<List<ChatsterListViewModel>> = Transformations.map(_members) {
+        it.map { it.toListViewModel() }
+    }
+
+    private val _loadingBar: MutableLiveData<Boolean> = MutableLiveData(false)
+    val loadingBar: LiveData<Boolean> = _loadingBar
 
     init {
         getMemberList()
     }
 
     private fun getMemberList() {
+        _loadingBar.value = true
         val user = realmSync.currentUser() ?: return
         val config = SyncConfiguration.Builder(user, "all-users=all-the-users").build()
-
         Realm.getInstanceAsync(config, object : Realm.Callback() {
             override fun onSuccess(realm: Realm) {
+                _loadingBar.value = false
                 val users = realm.where<Chatster>().findAll().sort("displayName").map {
                     it
                 }
-                _members.value = users
+                _members.value = realm.copyFromRealm(users)
             }
 
             override fun onError(exception: Throwable) {
                 super.onError(exception)
+                _loadingBar.value = false
                 //TODO : need to implement
             }
         })
