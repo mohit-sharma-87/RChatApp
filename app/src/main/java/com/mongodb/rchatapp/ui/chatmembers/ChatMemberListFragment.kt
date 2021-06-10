7743d864e2540b5cfeefd332d3c3b4c4283ced22
Mohit Sharma <mohit7575@gmail.com>
2021-06-10 11:18:40 +0100

    Chat room creation impl

--- app/src/main/java/com/mongodb/rchatapp/ui/chatmembers/ChatMemberListFragment.kt
+++ app/src/main/java/com/mongodb/rchatapp/ui/chatmembers/ChatMemberListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.mongodb.rchatapp.databinding.FragmentMemberListBinding
+import com.mongodb.rchatapp.ui.data.CreateNewChatNavigation
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 /**
@@ -15,7 +17,6 @@ class ChatMemberListFragment : Fragment() {
 
     private val viewModel: ChatMemberViewModel by viewModel()
     private lateinit var binding: FragmentMemberListBinding
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +40,20 @@ class ChatMemberListFragment : Fragment() {
         viewModel.members.observe(viewLifecycleOwner) {
             val adapter = binding.rvMemberList.adapter as ChatMemberViewAdapter
             adapter.updateMemberList(it)
+        }
+
+        binding.fabAddRoom.setOnClickListener {
+            val adapter = binding.rvMemberList.adapter as ChatMemberViewAdapter
+            viewModel.createChatRoom(
+                roomName = binding.etRoomName.text.toString(),
+                selectedMembers = adapter.values.toList()
+            )
+        }
+
+        viewModel.navigation.observe(viewLifecycleOwner) {
+            when (it) {
+                CreateNewChatNavigation.GoToDashboard -> findNavController().navigateUp()
+            }
         }
     }
 
